99bb82695b5acfb74cc818eb724ddc5dfa939656
beaskoczy <beaskoczy@gmail.com>
2025-01-17 22:15:23 +0100

    Files added

---
+++ Data/Tocs/Cozy_Gamer_Starter_Pack.js
@@ -0,0 +1,1 @@
+define({numchunks:1,prefix:'Cozy_Gamer_Starter_Pack_Chunk',chunkstart:['/Content/Topics/1._What_Is_Cozy_Gaming.htm'],tree:{n:[{i:0,c:0},{i:1,c:0},{i:2,c:0},{i:3,c:0},{i:4,c:0},{i:5,c:0},{i:6,c:0},{i:7,c:0}]}});
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638727457122810324'
+});
